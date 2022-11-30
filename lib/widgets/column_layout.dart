df899eb107ad1871f9c46198ad232ea27e71d3de
atif-git <atifraza1430@gmail.com>
2022-11-30 19:17:37 +0530

    initial commit

---
+++ lib/widgets/column_layout.dart
@@ -0,0 +1,41 @@
+import 'package:book_tickets/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../utils/app_layout.dart';
+
+class AppColumnLayout extends StatelessWidget {
+  final CrossAxisAlignment alignment;
+  final String firstText;
+  final String secondText;
+  final bool? isColor;
+  const AppColumnLayout(
+      {super.key,
+      required this.firstText,
+      required this.secondText,
+      required this.alignment,
+      this.isColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: alignment,
+      children: [
+        Text(
+          firstText,
+          style: isColor == null
+              ? Styles.headLineStyle3.copyWith(color: Colors.white)
+              : Styles.headLineStyle3,
+        ),
+        Gap(AppLayout.getHeight(5)),
+        Text(
+          secondText,
+          style: isColor == null
+              ? Styles.headLineStyle4.copyWith(
+                  color: Colors.white,
+                )
+              : Styles.headLineStyle4,
+        ),
+      ],
+    );
+  }
+}
